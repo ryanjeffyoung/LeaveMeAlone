b179a763a427548d1ead3f10301d3e70582a09a8
Ryan Young <ryanjeffyoung@gmail.com>
2021-02-08 12:32:02 -0800

    maybe fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from setup import get_calendar_service
-from freebusy import getApiResult, getBusyList, fill_day,checkBusy, apiCall
+from setup import Setup
+from freebusy import getApiResult, getBusyList
 from event import SLL
 from datetimerange import DateTimeRange
 import datetime
@@ -7,41 +7,38 @@ import time
 import i2c_lcd_driver
 import pytz
 from pytz import timezone
-
 # init lcd screen
 mylcd = i2c_lcd_driver.lcd()
 mylcd.lcd_clear()
-
 # set calId to use
 calId = 'ryan.jeffery.young@gmail.com'
 # create google API service
-service = get_calendar_service()
-# todays_events = apiCall(service, calId) # this is resetting the head every cycle, endless loop
-# todays_events.output_list()
-
+service = Setup()
 while True:
     now = datetime.datetime.utcnow().isoformat() + 'Z'
-
     result = getApiResult(service, calId)
     events_list = getBusyList(result, calId)
     if len(events_list) == 0:
-        mylcd.lcd_display_string('Not busy', 1)
+        # mylcd.lcd_display_string('Not busy', 1)
+        # mylcd.lcd_display_string('Please knock :)', 2)
+        print('not busy')
     for event in events_list:
         start = event['start']
         end = event['end']
-
         pst = timezone('US/Pacific')
         end_dt = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%Sz')
         end_dt_utc = pytz.UTC.localize(end_dt)
         end_local = end_dt_utc.astimezone(pst)
-        end_formatted = end_local.strftime('%I:%M:%S %p')                                                           
+        end_formatted = end_local.strftime('%I:%M %p')                                                           
         time_range = DateTimeRange(start, end)
-
         if now in time_range:
             mylcd.lcd_display_string("BUSY Until", 1)
             mylcd.lcd_display_string(end_formatted, 2)
+            # print('busy until', end_formatted)
         else:
-            print('sleeping')
+            mylcd.lcd_display_string('Not busy', 1)
+            mylcd.lcd_display_string('Please knock :)', 2)
+            # print('not busy')
     # sleep for 1 min refresh
     time.sleep(60)
     
--- quickstart.py
+++ quickstart.py
@@ -1,12 +1,27 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START calendar_quickstart]
 from __future__ import print_function
 import datetime
-import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
-# If modifying these scopes, delete the file token.pickle.
+# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
@@ -15,12 +30,11 @@ def main():
     Prints the start and name of the next 10 events on the user's calendar.
     """
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
+    # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -30,8 +44,8 @@ def main():
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
 
@@ -52,3 +66,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# [END calendar_quickstart]
